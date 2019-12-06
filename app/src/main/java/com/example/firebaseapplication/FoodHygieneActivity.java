b91326271780d022f1fe73fff95cef77ba98dd46
Paul Leoanrd <leonard-p7@ulster.ac.uk>
2019-12-06 12:53:23 +0000

    food hygiene commit

--- app/src/main/java/com/example/firebaseapplication/FoodHygieneActivity.java
+++ app/src/main/java/com/example/firebaseapplication/FoodHygieneActivity.java
@@ -6,11 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONObject;
 
 public class FoodHygieneActivity extends AppCompatActivity
 {
     private FirebaseAuth firebaseAuth;
+    RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+    final String url = "http://api.ratings.food.gov.uk/Establishments/basic";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,4 +36,21 @@ public class FoodHygieneActivity extends AppCompatActivity
     {
         startActivity(new Intent(FoodHygieneActivity.this, MainActivity.class));
     }
+
+    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+            (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    // here you will get the restaurants and rating details
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // TODO: Handle error
+
+                }
+            });
+
 }
