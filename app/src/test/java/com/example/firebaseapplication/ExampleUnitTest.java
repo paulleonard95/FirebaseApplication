ab87e0c4211213a8623e7acc2d1c5f3d4702eaa5
Paul Leonard <leonard-p7@ulster.ac.uk>
2019-12-13 10:59:24 +0000

    adding in simple tests

--- app/src/test/java/com/example/firebaseapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/firebaseapplication/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.firebaseapplication;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,50 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private String email = "paul@gmail.com";
+    private String password = "Password1";
+    private String first_name = "Paul";
+    private String last_name = "Leonard";
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void checkEmailIsCorrect()
+    {
+        assertEquals(email, "paul@gmail.com");
+    }
+
+    @Test
+    public void checkEmailIsNotCorrect()
+    {
+        assertNotEquals(email, "pl@gmail.com");
+    }
+
+    @Test
+    public void checkIfPasswordIsCorrect()
+    {
+        assertEquals(password, "Password1");
+    }
+
+    @Test
+    public void checkIfNotCorrectPassword()
+    {
+        assertNotEquals(password, "Password");
+    }
+
+    @Test
+    public void checkIfFirtNameTextIsFilled()
+    {
+        assertNotNull(first_name);
+    }
+
+    @Test
+    public void checkIfLastNameTextIsFilled()
+    {
+        assertNotNull(last_name);
     }
 }
